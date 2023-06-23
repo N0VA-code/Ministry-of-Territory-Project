1fc8e91d2981c02eb8ccf5a74c62956b8839b208
Minseok Kim <80535193+N0VA-code@users.noreply.github.com>
2023-06-23 05:26:28 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,34 +5,36 @@ from statsmodels.tsa.arima.model import ARIMA
 
 app = Flask(__name__)
 
-@app.route('/forecast', methods=['GET'])
-def forecast():
+@app.route('/forecast/<int:row>', methods=['GET'])
+def forecast(row):
     # 데이터 로드
     df = pd.read_csv("/Users/kimminseok/Downloads/연간_통계_자치구별-11-15.txt", header=None, sep='\s+')
 
-    all_forecasts = []
-    all_dust_amounts = []
+    if row >= df.shape[0]:
+        return jsonify({'error': 'row out of range'}), 400
 
-    for row in range(df.shape[0]):
-        # 각 행에서 5년치 데이터를 추출하고 이를 사용해 모델을 학습
-        traffic_data = pd.to_numeric(df.iloc[row, :60].values, errors='coerce')  # 5년치 데이터
+    # 해당 행에서 5년치 데이터를 추출하고 이를 사용해 모델을 학습
+    traffic_data = pd.to_numeric(df.iloc[row, :60].values, errors='coerce')  # 5년치 데이터
 
-        # 모델 생성 및 학습
-        model = ARIMA(traffic_data, order=(5, 1, 0))
-        model_fit = model.fit()
+    # 모델 생성 및 학습
+    model = ARIMA(traffic_data, order=(5, 1, 0))
+    model_fit = model.fit()
 
-        # 예측: 다음 1년치 교통량
-        n_periods = 12
-        forecast = model_fit.forecast(steps=n_periods)[0]
-        all_forecasts.append(forecast.tolist())
+    # 예측: 다음 1년치 교통량
+    n_periods = 12
+    forecast_values = model_fit.forecast(steps=n_periods)
 
-        # 미세먼지량 계산
-        beta0 = 0  # 상수항
-        beta1 = 0.5  # 교통량에 대한 계수
-        dust_amount = 4*(beta0 + beta1 * forecast)  # 미세먼지량 계산
-        all_dust_amounts.append(dust_amount.tolist())
+    # 미세먼지량 계산
+    beta0 = 0  # 상수항
+    beta1 = 0.5  # 교통량에 대한 계수
+    dust_amount = 4 * (beta0 + beta1 * forecast_values)  # 미세먼지량 계산
 
-    return jsonify({'forecasts': all_forecasts, 'dust_amounts': all_dust_amounts})
+    # 각 예측값 및 미세먼지량 출력
+    print("Forecast: ", forecast_values)
+    print("Dust amount: ", dust_amount)
+
+    # 둘 다 list 형태로 반환
+    return jsonify({'forecast': forecast_values.tolist(), 'dust_amount': dust_amount.tolist()})
 
 if __name__ == '__main__':
     app.run(debug=True)

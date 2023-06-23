be8f10e41b33c7cf5254a824bec7c0c160e227ea
MKcodeHub <135332465+MKcodeHub@users.noreply.github.com>
2023-06-24 01:11:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/forecast/<int:row>', methods=['GET'])
 def forecast(row):
     # 데이터 로드
-    df = pd.read_csv("/Users/kimminseok/Downloads/연간_통계_자치구별-11-15.txt", header=None, sep='\s+')
+    df = pd.read_csv("/연간_통계_자치구별-11-15.txt", header=None, sep='\s+')
 
     if row >= df.shape[0]:
         return jsonify({'error': 'row out of range'}), 400
@@ -35,10 +35,6 @@ def forecast(row):
     beta0 = 0  # 상수항
     beta1 = 0.5  # 교통량에 대한 계수
     dust_amount = 4 * (beta0 + beta1 * forecast_values)  # 미세먼지량 계산
-
-    # 각 예측값 및 미세먼지량 출력
-    print("Forecast: ", forecast_values)
-    print("Dust amount: ", dust_amount)
 
     # 둘 다 list 형태로 반환
     return jsonify({'forecast': forecast_values.tolist(), 'dust_amount': dust_amount.tolist()})
